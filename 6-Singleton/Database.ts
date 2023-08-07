f68ea859dc9f9560d28ab5cdeb9c899668b50fc4
Lucas Ivisson <lucasivisson1@gmail.com>
2023-08-07 19:02:11 -0300

    ✨ introduce singleton

---
+++ 6-Singleton/Database.ts
@@ -0,0 +1,22 @@
+export class Database {
+  private static instance: Database;
+  private db: string;
+
+  private constructor(db: string) {
+    this.db = db;
+    console.log("Configuração do banco de dados concluída.");
+  }
+
+  public static getInstance(): Database {
+    if (!Database.instance) {
+      Database.instance = new Database("MySQL");
+    }
+    return Database.instance;
+  }
+
+  public query(sql: string): void {
+    console.log(
+      `Executando o sql: "${sql}" utilizando o banco de dados ${this.db}`
+    );
+  }
+}
